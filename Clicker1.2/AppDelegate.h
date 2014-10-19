26bfb2729b55922be7fe7da306a185af81658240
Alan <alan@Alans-MacBook-Pro.local>
2014-10-18 22:58:49 -0400

    Initial Commit

---
+++ Clicker1.2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Clicker1.2
+//
+//  Created by Alan on 10/18/14.
+//  Copyright (c) 2014 alanchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Clicker1.2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Clicker1.2
+//
+//  Created by Alan on 10/18/14.
+//  Copyright (c) 2014 alanchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
